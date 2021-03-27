b977df111cd5ad5272444feaeba0859b86030ec6
천수빈 <suebin710@gmail.com>
2021-03-27 23:27:03 +0900

    add naver auto login

--- DDUBUCK/SceneDelegate.swift
+++ DDUBUCK/SceneDelegate.swift
@@ -6,11 +6,17 @@
 //
 
 import UIKit
+import NaverThirdPartyLogin
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+      NaverThirdPartyLoginConnection
+        .getSharedInstance()?
+        .receiveAccessToken(URLContexts.first?.url)
+    }
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
