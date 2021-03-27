889683dee6af782545636077a1918b0637da98d3
천수빈 <suebin710@gmail.com>
2021-03-27 22:31:06 +0900

    initial commit

---
+++ DDUBUCK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DDUBUCK
+//
+//  Created by exception on 2021/03/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var navBar: UINavigationBar!
+    
+    override func viewDidLoad() {
+        navBar.topItem?.title = "제목제목!!"
+    }
+}
+
